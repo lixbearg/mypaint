02814d22efc24d345649e9ee2fe279df6a209659
Danilo F. Silva <danilofes@gmail.com>
2015-05-11 21:21:11 -0300

    Exemplo de código para criar Toolbar e botão.

--- poo-paint/src/principal/Paint.java
+++ poo-paint/src/principal/Paint.java
@@ -1,6 +1,12 @@
 package principal;
 
+import java.awt.BorderLayout;
+
+import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JToggleButton;
+import javax.swing.JToolBar;
 
 import figuras.Circulo;
 import figuras.Figura;
@@ -13,13 +19,29 @@ public class Paint extends JFrame {
 	}
 
 	public Paint() {
+		JPanel painelPrincipal = new JPanel();
+		painelPrincipal.setLayout(new BorderLayout());
+		
+		JToolBar barraDeFerramentas = new JToolBar("Barra de Ferramentas");
+		painelPrincipal.add(barraDeFerramentas, BorderLayout.PAGE_START);
+		
+		ButtonGroup btnGroup = new ButtonGroup();
+		JToggleButton botaoRet = new JToggleButton("Retângulo");
+		//botaoRet.addActionListener(/* Aqui vem um ActionListener */);
+		botaoRet.getModel().setPressed(true);
+		btnGroup.add(botaoRet);
+		barraDeFerramentas.add(botaoRet);
+		barraDeFerramentas.setFloatable(false);
+		
 		Retangulo ret1 = new Retangulo(5, 5, 100, 80);
 		Retangulo ret2 = new Retangulo(400, 150, 30, 30);
 		Circulo c = new Circulo(200, 200, 56);
-		
 		Figura[] figuras = new Figura[]{ret1, ret2, c};
+		
 		AreaDeDesenho areaDeDesenho = new AreaDeDesenho(figuras);
-		this.setContentPane(areaDeDesenho);
+		painelPrincipal.add(areaDeDesenho, BorderLayout.CENTER);
+		
+		this.setContentPane(painelPrincipal);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.pack();
 		this.setVisible(true);
